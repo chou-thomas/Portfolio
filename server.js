89eda8d8a4f70750b56d7fc7cf65f5369706012e
chou.thomas <chou.thomas3@gmail.com>
2016-02-22 20:24:47 -0800

    bootstrap/main files

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
+
+app.get('/', function(req,res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+// require('./assets/css/main.css');
+app.use(express.static('assets'));
+
 var port =  Number(process.env.PORT || 8000)
 server.listen(port);
 console.log('listening on port 8000');
 
-app.get('/', function(req,res){
-	res.sendfile(__dirname + '/index.html');
-});
+// app.get('/', function(req,res){
+// 	res.sendFile(__dirname + '/index.html');
+// });
